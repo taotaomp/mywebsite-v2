fb19180289c14ab63e6d111c69bdacd9054d5335
Pudding Add pudding <hu1song2tao3@gmail.com>
2020-10-18 16:48:12 +0800

    header增加token

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,4 +5,17 @@ const service = axios.create({
     timeout:10000
 });
 
+service.interceptors.request.use(
+    config => {
+      const token = localStorage.getItem('token') 
+      if (token) { // 判断是否存在token，如果存在的话，则每个http header都加上token
+        console.log('add header')
+        config.headers.Authorization ='Bearer ' + token  //请求头加上token
+      }
+      return config
+    },
+    err => {
+      return Promise.reject(err)
+    })
+
 export default service
